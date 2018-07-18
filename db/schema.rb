9b10b751f42c38d1883ba125389c0dd4db8450ad
Joonho Choi <joonho0645@gmail.com>
2018-07-18 15:13:25 +0800

    crud complete

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20180717081345) do
+ActiveRecord::Schema.define(version: 20180718071231) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -50,10 +50,13 @@ ActiveRecord::Schema.define(version: 20180717081345) do
     t.inet "last_sign_in_ip"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.bigint "room_id"
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
+    t.index ["room_id"], name: "index_users_on_room_id"
   end
 
   add_foreign_key "chats", "rooms"
   add_foreign_key "rooms", "users"
+  add_foreign_key "users", "rooms"
 end
